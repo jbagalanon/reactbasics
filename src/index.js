c6f088d53595134c0e2de27e6c08a976b10622b1
Joemar Bagalanon <31525007+jbagalanon@users.noreply.github.com>
2022-04-13 01:23:12 +0800

    jsx rules

--- src/index.js
+++ src/index.js
@@ -2,17 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
+function Greeting() {
+  return (
+    <div>
+      <h3>Hello Everybody</h3>
+      <ul>
+        <li>
+          <a href="#">Jomskie</a>
+        </li>
+      </ul>
+    </div>
+  );
+}
 
-// function Greeting() {
-//   return (
-//     <div>
-//       <h4> Hi Jomskieq</h4>
-//     </div>);
-// }
-
-const Greeting = () => {
-  return React.createElement('div', {}, React.createElement('h1', {}, "Hello Jomskie"));
-};
+// const Greeting = () => {
+//   return React.createElement('div', {}, React.createElement('h1', {}, "Hello Jomskie"))
+// };
 
 
 ReactDOM.render(<Greeting />, document.getElementById('root'));
