9f48e0d48595166008a8893efbe0daa4758eae87
Joemar Bagalanon <31525007+jbagalanon@users.noreply.github.com>
2022-04-13 13:46:07 +0800

    implementing inline css

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-
-
 import './index.css'; //importing file
-
 
 function BookList() {
   return (<section class='booklist'>
@@ -29,10 +26,9 @@ const Image = () => (
   <img src="https://images-na.ssl-images-amazon.com/images/I/91YKVfaKDsL._AC_UL210_SR195,210_.jpg" alt="" />
 );
 
-const Title = () => (<h1>Teletubbies</h1>
-);
+const Title = () => <h1>Teletubbies</h1>;
 
-const Author = () => <h2>Swordfish</h2>;
+const Author = () => <h4 style={{ color: '#617d98', fontSize: '.75rem', marginTop: '.25rem' }}>Swordfish</h4>;
 
 
 
