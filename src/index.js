267904b6e60331554f0f61294c2ea88eff72754e
Joemar Bagalanon <31525007+jbagalanon@users.noreply.github.com>
2022-04-13 16:29:51 +0800

    Props done

--- src/index.js
+++ src/index.js
@@ -5,30 +5,41 @@ import ReactDOM from 'react-dom';
 import './index.css'; //importing file
 
 //variable declarition
-const author = "Bhugzfire";
-const title = "Teletubbies Return";
-const img = "https://images-na.ssl-images-amazon.com/images/I/91YKVfaKDsL._AC_UL210_SR195,210_.jpg";
 
+const firstBook = {
+  img: "https://images-na.ssl-images-amazon.com/images/I/91YKVfaKDsL._AC_UL210_SR195,210_.jpg",
+  title: "Teletubbies Return",
+  author: "Bhugzfire"
+
+}
+
+const secondBook = {
+  img: "https://images-na.ssl-images-amazon.com/images/I/51al5c5C52L._SX365_BO1,204,203,200_.jpg",
+  title: "It's not easy being bunny",
+  author: "Walt Disney"
+
+}
 
 function BookList() {
   return (<section class='booklist'>
-    <Book />
-
-
+    <Book
+      img={firstBook.img}
+      title={firstBook.title}
+      author={firstBook.author} />
+    <Book 
+      img={secondBook.img}
+      title={secondBook.title}
+      author={secondBook.author} />
   </section>
   );
 }
 
-
-
-const Book = () => {
-
-
+const Book = (props) => {
   return <article className='book'>
-    <img src={img} alt="" />
-
-    <h1>{title}</h1>
-    <h4>{author}</h4>
+    <img src={props.img} alt="" />
+    <h1>{props.title}</h1>
+    <h4>{props.author}</h4>
+    
   </article>;
 };
 
