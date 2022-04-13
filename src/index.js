2969027a3d0c670fb07a3cf9f6ae6ed4cf05f7d8
Joemar Bagalanon <31525007+jbagalanon@users.noreply.github.com>
2022-04-13 14:17:04 +0800

    implementing jsx

--- src/index.js
+++ src/index.js
@@ -6,29 +6,27 @@ import './index.css'; //importing file
 function BookList() {
   return (<section class='booklist'>
     <Book />
-    <Book />
-    <Book />
-    <Book />
-    <Book />
+
 
   </section>
   );
 }
 
+const author = "Bhugzfire";
+
 const Book = () => {
-  return <article className='book'><Image></Image>
-    <Title />
-    <Author />
+  
+  const title = "Teletubbies Return"
+  return <article className='book'>
+    <img src="https://images-na.ssl-images-amazon.com/images/I/91YKVfaKDsL._AC_UL210_SR195,210_.jpg" alt="" />
+
+    <h1>{title}</h1>
+    <h4>{author.toUpperCase()}</h4>
+    {/* <p>{let x = 6}</p> */}
   </article>;
 };
 
-const Image = () => (
-  <img src="https://images-na.ssl-images-amazon.com/images/I/91YKVfaKDsL._AC_UL210_SR195,210_.jpg" alt="" />
-);
 
-const Title = () => <h1>Teletubbies</h1>;
-
-const Author = () => <h4 style={{ color: '#617d98', fontSize: '.75rem', marginTop: '.25rem' }}>Swordfish</h4>;
 
 
 
